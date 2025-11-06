69da1739ebbe64610548cee6714e34cc0d819c47
tanya brassie <tanya.brassie@gmail.com>
2025-11-06 14:34:07 -0500

    Migrate CSS to SCSS; update imports; splash page layout with tiled Clarkson Bot gif; remove CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { SplashPage } from "./components/Splashpage";
 
 function App() {
--- src/components/Splashpage/index.tsx
+++ src/components/Splashpage/index.tsx
@@ -1,3 +1,23 @@
+import "./index.scss";
+import clarksonBotUrl from "../../assets/clarkson-bot.gif";
+
 export const SplashPage = () => {
-  return <div>hi</div>;
+  return (
+    <div
+      className="splash"
+      style={{
+        // Tile the animated bot as the page background
+        backgroundImage: `url(${clarksonBotUrl})`,
+      }}
+    >
+      <div className="splash__center">
+        <img
+          className="splash__bot"
+          src={clarksonBotUrl}
+          alt="Clarkson Bot"
+          decoding="async"
+        />
+      </div>
+    </div>
+  );
 };
