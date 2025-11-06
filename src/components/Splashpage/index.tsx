9e96d071ca5d783569b61cf454f0066e8425b4ee
tanya brassie <tanya.brassie@gmail.com>
2025-11-06 15:03:50 -0500

    Updating animation

--- src/components/Splashpage/index.tsx
+++ src/components/Splashpage/index.tsx
@@ -3,13 +3,13 @@ import clarksonBotUrl from "../../assets/clarkson-bot.gif";
 
 export const SplashPage = () => {
   return (
-    <div
-      className="splash"
-      style={{
-        // Tile the animated bot as the page background
-        backgroundImage: `url(${clarksonBotUrl})`,
-      }}
-    >
+    <div className="splash">
+      <div
+        className="splash__bg"
+        style={{
+          backgroundImage: `url(${clarksonBotUrl})`,
+        }}
+      />
       <div className="splash__center">
         <img
           className="splash__bot"
