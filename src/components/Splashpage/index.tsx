98ab7b6ab9fce810af56a4d3df4c2038c6db778e
tanya brassie <tanya.brassie@gmail.com>
2025-11-07 11:12:07 -0500

    clarksonbot word animation

--- src/components/Splashpage/index.tsx
+++ src/components/Splashpage/index.tsx
@@ -4,6 +4,11 @@ import clarksonBotUrl from "../../assets/clarkson-bot.gif";
 export const SplashPage = () => {
   return (
     <div className="splash">
+      <h1 className="splash__title">
+        <span className="splash__title-word splash__title-word--1">CLARK</span>
+        <span className="splash__title-word splash__title-word--2">SON</span>
+        <span className="splash__title-word splash__title-word--3">BOT</span>
+      </h1>
       <div
         className="splash__bg"
         style={{
