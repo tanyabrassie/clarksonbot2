f2142c5bc0c75ae7f525a9173fa22793730d3aae
tanya brassie <tanya.brassie@gmail.com>
2025-11-10 15:21:57 -0500

    Fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -60,8 +60,6 @@ function App() {
                   <BuildABot />
                 </CenterContent>
                 <RightContent></RightContent>
-                <Marquee />
-                <BottomBanner />
               </>
             }
           />
