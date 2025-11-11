e0d2c1a58602d64eb729c159735b5689fe3518bb
tanya brassie <tanya.brassie@gmail.com>
2025-11-10 23:02:54 -0500

    offering coin

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from "./style.module.scss";
 import { HeaderContent } from "./HeaderContent";
 import { BottomBanner } from "../BottomBanner/BottomBanner";
+import retroCoinUrl from "../../assets/retro_coin.png";
 
 export const Header = () => {
   return (
@@ -8,6 +9,25 @@ export const Header = () => {
       <BottomBanner />
       <header className={styles.headerContainer}>
         <HeaderContent />
+        <div className={styles.coinContainer}>
+          <img
+            src={retroCoinUrl}
+            alt="Retro Coin"
+            className={styles.retroCoin}
+          />
+          <span className={styles.coinSparkle} data-sparkle="1">
+            ✨
+          </span>
+          <span className={styles.coinSparkle} data-sparkle="2">
+            ✨
+          </span>
+          <span className={styles.coinSparkle} data-sparkle="3">
+            ✨
+          </span>
+          <span className={styles.coinSparkle} data-sparkle="4">
+            ✨
+          </span>
+        </div>
       </header>
       <BottomBanner />
     </>
