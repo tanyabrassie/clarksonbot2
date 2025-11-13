5b199aae8a2435eff1645ccf891365a1d9debaa7
tanya brassie <tanya.brassie@gmail.com>
2025-11-12 21:27:53 -0500

    Refinements

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -4,17 +4,17 @@ export const Marquee = () => {
   return (
     <div>
       {/* @ts-expect-error - marquee is deprecated but works for retro aesthetic */}
-      <marquee className={styles.marquee}>
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
-        Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+    <marquee className={styles.marquee}>
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
+      Build A Bot Now Live! Try It Today! Build A Bot Now Live! Try It Today!
         {/* @ts-expect-error - closing marquee tag */}
-      </marquee>
+    </marquee>
     </div>
   );
 };
